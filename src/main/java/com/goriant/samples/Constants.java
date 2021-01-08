7239cbbca9df025f42fbf415e99bd08379a0c1e3
lamle <lam.le@mservice.com.vn>
2021-01-08 15:33:23 +0700

    remove unused constants

--- src/main/java/com/goriant/samples/Constants.java
+++ src/main/java/com/goriant/samples/Constants.java
@@ -7,10 +7,5 @@ public final class Constants {
 
     public static final String BREAK_LINE = "======================================";
     public static final String CONFIG_PATH = "app.yaml";
-    public static final String HRM_URL = "https://hrm.mservice.com.vn/";
-    public static final String USER_NAME = "UserName";
-    public static final String PASSWORD = "Password";
-    public static final String OTP_ID = "otp";
-
     public static final String OTP_REGEX = "\\\\\"\\d{6}\\\\\"";
 }
