6c402c59650b464dd5a7e933f21fbf541ced719c
lamle <lam.le@mservice.com.vn>
2021-02-15 15:59:35 +0700

    refactor: call telegram bot after submit OTP with response status

--- src/main/java/com/goriant/samples/service/LoginService.java
+++ src/main/java/com/goriant/samples/service/LoginService.java
@@ -4,32 +4,41 @@ import com.goriant.samples.AppConfig;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
 public class LoginService {
     private static final Logger log = LoggerFactory.getLogger(LoginService.class);
     private static final LoginService loginService = new LoginService();
+    private static TelegramBot telegramBot;
 
     public static LoginService getInstance() {
         return loginService;
     }
 
-    public String getVerificationToken(AppConfig config) throws Exception {
+    public String getVerificationToken(AppConfig config) throws IOException {
         Document doc = Jsoup.connect(config.getLoginUrl())
                 .headers(config.getLoginHeaders())
                 .get();
         Element element = doc.selectFirst("input[name=__RequestVerificationToken]");
 
-        log.info("Verification Token : {}", element.val());
+        String requestVerificationTokenValue = element.val();
+        log.info("Verification Token : {}", requestVerificationTokenValue);
 
-        return element.val();
+        return requestVerificationTokenValue;
     }
 
-    public void login(AppConfig config, String verificationToken) throws Exception {
+    public void login(AppConfig config, String verificationToken) throws UnirestException {
         String body = String.format("UserName=%s&Password=%s&__RequestVerificationToken=%s", config.getUser(), config.getPassword(), verificationToken);
 
         log.info("Request body: {}", body.replace(config.getPassword(), "******"));
@@ -42,7 +51,7 @@ public class LoginService {
         log.info(response.getBody());
     }
 
-    public void submitOtp(AppConfig config, String otp) throws Exception {
+    public void submitOtp(AppConfig config, String otp) throws UnirestException {
         String body = String.format("emailAddress=%s&otp=%s", config.getUser(), otp);
         log.info(body);
 
@@ -50,7 +59,26 @@ public class LoginService {
                 .headers(config.getLoginHeaders())
                 .body(body)
                 .asString();
+        String res = response.getBody();
+        log.info("Submit OTP status : {}", res);
 
-        log.info("Submit OTP status : {}", response.getBody());
+        String msg = buildTelegramMsg(config, otp, res);
+        log.info("Call telegram chat bot with msg : {}", msg);
+
+        getTelegramBot(config.getTelegramToken())
+                .execute(new SendMessage(config.getTelegramChatId(), msg));
+    }
+
+    private String buildTelegramMsg(AppConfig config, String otp, String res) {
+        return String.format("[%s] %s OTP is: %s - with HRM response %s",
+                LocalDateTime.now().withNano(0).format(ISO_LOCAL_DATE_TIME),
+                config.getUser(), otp, res);
+    }
+
+    private static TelegramBot getTelegramBot(String teleToken) {
+        if (null == telegramBot) {
+            telegramBot = new TelegramBot(teleToken);
+        }
+        return telegramBot;
     }
 }
--- src/main/java/com/goriant/samples/service/OtpCollector.java
+++ src/main/java/com/goriant/samples/service/OtpCollector.java
@@ -6,21 +6,15 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.mashape.unirest.request.HttpRequestWithBody;
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.LocalDateTime;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
 public class OtpCollector {
     private static final Logger log = LoggerFactory.getLogger(OtpCollector.class);
     private static final Pattern otpPattern = Pattern.compile(Constants.OTP_REGEX);
-    private static TelegramBot telegramBot;
 
     private static final OtpCollector OTP_COLLECTOR = new OtpCollector();
 
@@ -34,17 +28,8 @@ public class OtpCollector {
         otpRequest.body(config.getChatRequestBody());
         HttpResponse<String> otpResponse = otpRequest.asString();
         String otp = extractOtp(otpResponse.getBody());
-
-        String msg = String.format("[%s] %s OTP is: %s", LocalDateTime.now().withNano(0).format(ISO_LOCAL_DATE_TIME), config.getUser(), otp);
-        getTelegramBot(config.getTelegramToken())
-                .execute(new SendMessage(config.getTelegramChatId(), msg));
         log.info("OTP : {}", otp);
         return otp;
-    }
-
-    private TelegramBot getTelegramBot(String teleToken) {
-        if (null == telegramBot) return new TelegramBot(teleToken);
-        return telegramBot;
     }
 
     private String extractOtp(String body) {
