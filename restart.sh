4f57dab3fbf0b1e2f62d97fb246d5007cb40de2d
lamle <lam.le@mservice.com.vn>
2021-05-26 11:13:40 +0700

    add notes

--- restart.sh
+++ restart.sh
@@ -1,3 +1,9 @@
+echo "------------------------------------------------------------------"
+echo "Nhớ update Google Session ở file curl_chat.txt nha các tình yêu :D"
+echo "------------------------------------------------------------------"
+
+sleep 3
+
 mvn clean install -DskipTests
 kill -9 `ps aux | grep samples.jar | awk '{print $2}'`
 nohup java -jar target/samples.jar &
